4be6716fb1f15a869c285664476192e035ac426f
Gabriel Machado <gabrielc.machado@hotmail.com>
2025-04-16 23:05:27 -0300

    ENVIRONMENT_VARIABLE_FRONT_END_URL adicionado.

--- src/BlogApi.API/Program.cs
+++ src/BlogApi.API/Program.cs
@@ -33,8 +33,10 @@ builder.Services.ConfigureIdentity(builder.Configuration);
 var app = builder.Build();
 var DATA_DATABASE = app.Configuration.GetConnectionString("DATA_DATABASE");
 var IDENTITY_DATABASE = app.Configuration.GetConnectionString("IDENTITY_DATABASE");
+var ENVIRONMENT_VARIABLE_FRONT_END_URL = app.Configuration["ENVIRONMENT_VARIABLE_FRONT_END_URL"];
 Console.WriteLine($"DATA_DATABASE: {DATA_DATABASE}");
 Console.WriteLine($"IDENTITY_DATABASE: {IDENTITY_DATABASE}");
+Console.WriteLine($"ENVIRONMENT_VARIABLE_FRONT_END_URL: {ENVIRONMENT_VARIABLE_FRONT_END_URL}");
 
 
 app.UseStaticFiles();
@@ -65,84 +67,84 @@ app.UseAuthorization();
  
 app.MapControllers();
 
-// Verifica se o arquivo de marcação da primeira execução existe
-if (!File.Exists("blog_migration_marker.txt"))
-{
-    // Rodar as migrações automaticamente na primeira execução
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<BlogDbContext>();
-        await dbContext.Database.MigrateAsync();
-    }
+//// Verifica se o arquivo de marcação da primeira execução existe
+//if (!File.Exists("blog_migration_marker.txt"))
+//{
+//    // Rodar as migrações automaticamente na primeira execução
+//    using (var scope = app.Services.CreateScope())
+//    {
+//        var dbContext = scope.ServiceProvider.GetRequiredService<BlogDbContext>();
+//        await dbContext.Database.MigrateAsync();
+//    }
 
-    // Cria um arquivo para marcar que as migrações já foram executadas
-    File.Create("blog_migration_marker.txt").Dispose();
-}
-
-
-// Verifica se o arquivo de marcação da primeira execução existe
-if (!File.Exists("identity_migration_marker.txt"))
-{
-    // Rodar as migrações automaticamente na primeira execução
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<IdentityDbContext>();
-        await dbContext.Database.MigrateAsync();
-    }
-
-    // Cria um arquivo para marcar que as migrações já foram executadas
-    File.Create("identity_migration_marker.txt").Dispose();
-}
+//    // Cria um arquivo para marcar que as migrações já foram executadas
+//    File.Create("blog_migration_marker.txt").Dispose();
+//}
 
 
-using (var scope = app.Services.CreateScope())
-{
-    var roleSeeder = scope.ServiceProvider.GetRequiredService<RoleSeeder>();
-    await roleSeeder.SeedAsync();
+//// Verifica se o arquivo de marcação da primeira execução existe
+//if (!File.Exists("identity_migration_marker.txt"))
+//{
+//    // Rodar as migrações automaticamente na primeira execução
+//    using (var scope = app.Services.CreateScope())
+//    {
+//        var dbContext = scope.ServiceProvider.GetRequiredService<IdentityDbContext>();
+//        await dbContext.Database.MigrateAsync();
+//    }
 
-    // Caminho do arquivo marker
-    var rolemarkerFilePath = Path.Combine("/app/migrations", "role_seeder_marker.txt");
-
-    // Verifica se o marker já foi criado
-    if (!File.Exists(rolemarkerFilePath))
-    {
-        // Cria o arquivo marker indicando que o processo foi concluído
-        File.WriteAllText(rolemarkerFilePath, "RoleSeeder process completed.");
-    }
-    else
-    {
-        // Caso já tenha sido criado, apenas loga a informação
-        Console.WriteLine("RoleSeeder marker already exists. Process already completed.");
-    }
-}
+//    // Cria um arquivo para marcar que as migrações já foram executadas
+//    File.Create("identity_migration_marker.txt").Dispose();
+//}
 
 
-// Código original
-var mediator1 = app.Services.CreateScope().ServiceProvider.GetRequiredService<IMediator>();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var roleSeeder = scope.ServiceProvider.GetRequiredService<RoleSeeder>();
+//    await roleSeeder.SeedAsync();
 
-var tenancy = new CreateTenancyCommand
-{
-    Name = "Ozos",
-    Email = "contato@ozos.com.br",
-    Url = "ozos.com.br"
-};
+//    // Caminho do arquivo marker
+//    var rolemarkerFilePath = Path.Combine("/app/migrations", "role_seeder_marker.txt");
 
-var tenancyResult = await mediator1.Send(tenancy);
+//    // Verifica se o marker já foi criado
+//    if (!File.Exists(rolemarkerFilePath))
+//    {
+//        // Cria o arquivo marker indicando que o processo foi concluído
+//        File.WriteAllText(rolemarkerFilePath, "RoleSeeder process completed.");
+//    }
+//    else
+//    {
+//        // Caso já tenha sido criado, apenas loga a informação
+//        Console.WriteLine("RoleSeeder marker already exists. Process already completed.");
+//    }
+//}
 
-// Caminho do arquivo marker
-var tenancyMarkerFilePath = Path.Combine("/app/migrations", "tenancy_marker.txt");
 
-// Verifica se o marker já foi criado
-if (!File.Exists(tenancyMarkerFilePath))
-{
-    // Cria o arquivo marker indicando que o processo foi concluído
-    File.WriteAllText(tenancyMarkerFilePath, "Tenancy process completed.");
-}
-else
-{
-    // Caso já tenha sido criado, apenas loga a informação
-    Console.WriteLine("Tenancy marker already exists. Process already completed.");
-}
+//// Código original
+//var mediator1 = app.Services.CreateScope().ServiceProvider.GetRequiredService<IMediator>();
+
+//var tenancy = new CreateTenancyCommand
+//{
+//    Name = "Ozos",
+//    Email = "contato@ozos.com.br",
+//    Url = "ozos.com.br"
+//};
+
+//var tenancyResult = await mediator1.Send(tenancy);
+
+//// Caminho do arquivo marker
+//var tenancyMarkerFilePath = Path.Combine("/app/migrations", "tenancy_marker.txt");
+
+//// Verifica se o marker já foi criado
+//if (!File.Exists(tenancyMarkerFilePath))
+//{
+//    // Cria o arquivo marker indicando que o processo foi concluído
+//    File.WriteAllText(tenancyMarkerFilePath, "Tenancy process completed.");
+//}
+//else
+//{
+//    // Caso já tenha sido criado, apenas loga a informação
+//    Console.WriteLine("Tenancy marker already exists. Process already completed.");
+//}
 
 //var user = new CreateUserCommand()
 //{
